a40abf365affbe2071080fa8835d35bf2cf08c4a
Mike Xiong <mikazian@mbp-de-mike.home>
2024-05-31 14:48:24 +0200

    config vite and nginx

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    host: '0.0.0.0',
-    port: 4444,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-})
+export default defineConfig(({ mode }) => {
+  /**
+   * TODO: /!\ FRONTEND_PORT est undefined
+   */
+  const { FRONTEND_PORT } = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: +FRONTEND_PORT || 4444,
+    },
+    build: {
+      sourcemap: true,
+    },
+  };
+});
